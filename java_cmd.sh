7ee80e61c8a0eced65e9507d53e25828b979c45b
Will Roddy <wroddy@c-path.org>
2025-06-25 16:02:58 -0700

    update test cases and results

--- java_cmd.sh
+++ java_cmd.sh
@@ -1,2 +1,3 @@
 java -jar validator_cli.jar patient-test-cases/Patient_0001.json -transform PersonMap -ig https://build.fhir.org/ig/HL7/fhir-omop-ig -output transformed-omop/Output_0001.json
 java -jar validator_cli.jar patient-test-cases/Patient_0002.json -transform PersonMap -ig https://build.fhir.org/ig/HL7/fhir-omop-ig -output transformed-omop/Output_0002.json
+java -jar validator_cli.jar patient-test-cases/Condition_0003.json -transform ConditionMap -ig https://build.fhir.org/ig/HL7/fhir-omop-ig -output transformed-omop/Output_0003.json
